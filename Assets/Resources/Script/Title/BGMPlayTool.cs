aad794af873d3f76243df33e59ecc5d94db4df29
Nanaya <nwserika.nanaya@gmail.com>
2016-12-06 11:08:44 +0900

    BGM変更ツール追加

---
+++ Assets/Resources/Script/Title/BGMPlayTool.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMPlayTool : SingletonMonoBehaviour<BGMPlayTool> 
+{
+
+
+    void Awake()
+    {
+        //シングルトンでインスタンス生成
+        if (this != Instance)
+        {
+            //破棄
+            Destroy(this);
+            return;
+        }
+
+        //オブジェクトをシーン間え破棄しない
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        BGMPlay();
+    }
+
+    void BGMPlay()
+    {
+      //  Debug.Log(SceneManage.Instance.GetCurrentSceneName());
+        switch (SceneManage.Instance.GetCurrentSceneName())
+        {
+            case "Title":
+                {
+                    AudioManager.Instance.PlayBGM("title");
+                    break;
+                }
+            case "Operate":
+                {
+                   // AudioManager.Instance.PlayBGM("title");
+                    break;
+                }
+            case "Game":
+                {
+                    AudioManager.Instance.PlayBGM("game");
+                    break;
+                }
+            case "Clear":
+                {
+                    AudioManager.Instance.PlayBGM("risult");
+                    break;
+                }
+            case "GameOver":
+                {
+                    AudioManager.Instance.PlayBGM("gameover");
+                    break;
+                }
+            case "Result":
+                {
+                    //AudioManager.Instance.PlayBGM("title");
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+
+}
