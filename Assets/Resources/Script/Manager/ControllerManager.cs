7195a5c02f82081c93749db268c64e59c00fe520
mausya <aoisuisei3246@yahoo.co.jp>
2016-12-18 20:27:39 +0900

    なんかあったんで

---
+++ Assets/Resources/Script/Manager/ControllerManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerManager : SingletonMonoBehaviour<ScoreManager>
+{
+
+    public void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public float GetAxisForward()
+    {
+
+        return 0.0f;
+    }
+    public float GetAxisSide()
+    {
+
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
