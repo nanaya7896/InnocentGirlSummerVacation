deb8f35159c126abf3934e5f74bfa23a1096f232
Nanaya <nwserika.nanaya@gmail.com>
2017-01-17 00:46:46 +0900

    ScoreをUIに適用

---
+++ Assets/Score.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+public class Score : MonoBehaviour 
+{
+
+	public GameObject ten;
+	public GameObject one;
+
+	[SerializeField]
+	List<Sprite> sp = new List<Sprite>();
+	//時間を描画するためのSpriteRendererコンポーネントを持つGameObject
+	[SerializeField]
+	List<GameObject> once = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+		foreach(Sprite spr in Resources.LoadAll<Sprite>("Image/Number"))
+		{
+			sp.Add(spr);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int sco = ScoreManager.Instance.Score;
+		int tmp = sco / 10;
+		ten.GetComponent<Image>().sprite =sp[tmp];
+		one.GetComponent<Image> ().sprite = sp [(int)sco % 10];
+		//ten.GetComponent<Image>().mainTexture
+	}
+}
