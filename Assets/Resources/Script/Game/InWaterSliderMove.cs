eef81032306a1de7a817421235960520a39e430e
mausya <aoisuisei3246@yahoo.co.jp>
2017-01-17 15:09:54 +0900

    コントローラー横移動修正

---
+++ Assets/Resources/Script/Game/InWaterSliderMove.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ウォータースライダーに入った時の処理（コンポーネントを入れた地点で機能します）
+/// </summary>
+public class InWaterSliderMove : MonoBehaviour {
+
+    //ウォータースライダーの滑るときにかける時間
+    float time = 10.0f;
+
+    Vector3 rotate;
+    iTween itweenCmp;
+	// Use this for initialization
+	void Start () {
+
+        Hashtable moveHash = new Hashtable();
+
+        moveHash.Add("time", time);
+        moveHash.Add("path", iTweenPath.GetPath("WaterSlider1"));
+        moveHash.Add("easetype", iTween.EaseType.easeInQuad);
+        moveHash.Add("orienttopath", true);
+
+        iTween.MoveTo(this.gameObject, moveHash);
+        itweenCmp = this.GetComponent<iTween>();
+
+        rotate = this.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (itweenCmp == null)
+        {
+            Destroy(this);
+        }
+	}
+
+    void OnDestroy()
+    {
+        this.transform.eulerAngles = new Vector3(rotate.x,this.transform.eulerAngles.y,rotate.z);
+    }
+}
