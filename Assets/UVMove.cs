cd1d19fb5c75f977a5e50708619bb986508c7768
Nanaya <nwserika.nanaya@gmail.com>
2017-01-26 14:32:23 +0900

    水のテクスチャのUV値動かしたよ

---
+++ Assets/UVMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UVMove : MonoBehaviour {
+
+	public Rect rect;
+
+	private Material _material;
+
+	private int texWidth, texHeight;
+
+	// Use this for initialization
+	void Start () {
+		_material = this.GetComponent<Renderer> ().material;
+
+		Texture texture = _material.mainTexture;
+
+		texWidth = texture.width;
+		texHeight = texture.height;
+
+
+	}
+
+	[SerializeField]
+	Vector2 offset;
+	// Update is called once per frame
+	void Update () {
+		//rect.x += 0.001f;
+		rect.y += 0.1f;
+		offset = new Vector2 (rect.x, rect.y);
+		_material.SetTextureOffset ("_MainTex", offset);
+
+	}
+}
