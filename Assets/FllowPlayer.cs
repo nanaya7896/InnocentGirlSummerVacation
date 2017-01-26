0772f3a3c9aea5f6bd0171a5a23d11e9b6c058b2
Nanaya <nwserika.nanaya@gmail.com>
2017-01-26 13:54:25 +0900

    カメラ追跡完了

---
+++ Assets/FllowPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FllowPlayer : MonoBehaviour {
+	[SerializeField,Header("ゆきちゃん")]
+	Transform player=null;
+
+	Transform m_Player
+	{
+		get
+		{
+			if (player == null)
+			{
+				player = GameObject.FindGameObjectWithTag ("Player").transform;
+			}
+			return player;
+		}
+	}
+
+	[Header("カメラとプレイヤーの相対距離"),SerializeField]
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - m_Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = m_Player.transform.position + offset;
+	}
+}
