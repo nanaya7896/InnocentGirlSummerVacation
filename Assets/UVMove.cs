98d0bbf9cf2e25a59a5e62b9e3c0fa11eefda8db
Nanaya <nwserika.nanaya@gmail.com>
2017-01-26 14:37:29 +0900

    プールにも追加

--- Assets/UVMove.cs
+++ Assets/UVMove.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class UVMove : MonoBehaviour {
 
+	[Header("TextureのRect")]
 	public Rect rect;
-
+	[Header("UV値に加算する値")]
+	public float addValue;
+	[Header("動かしたいマテリアル")]
 	private Material _material;
 
 	private int texWidth, texHeight;
@@ -21,12 +24,12 @@ public class UVMove : MonoBehaviour {
 
 	}
 
-	[SerializeField]
+	[SerializeField,Header("Move用格納変数")]
 	Vector2 offset;
 	// Update is called once per frame
 	void Update () {
 		//rect.x += 0.001f;
-		rect.y += 0.1f;
+		rect.y += addValue;
 		offset = new Vector2 (rect.x, rect.y);
 		_material.SetTextureOffset ("_MainTex", offset);
 
